6761b7f493daf57ed9dafab401514455aaa6e125
Henrique Monteiro <henrique.mont011@gmail.com>
2020-06-14 23:27:59 -0300

    Fixed responsive layout

--- js/index.js
+++ js/index.js
@@ -16,3 +16,147 @@ window.onload = () => {
     return console.log('funciona');
   };
 };
+
+var adapter = new LocalStorage('db')
+var db = low(adapter)
+
+db.defaults({ items: [
+  {
+    name: 'Acidente de Carro',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Sangramento',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Doença Respiratória',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Doença Cardiaca',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Pressão Alta/Baixa',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Alergia',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Depressão',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Diabetes',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Asma',
+    sections: [
+      {
+        nome: 'Atropelamento',
+        articles: [
+          {
+            title: 'Conteudo Teste',
+            content: 'Conteudo do artigo',
+            image: 'image content'
+          }
+        ]
+      }
+    ]
+  },
+]}).write()
+
+console.log(db.get('items').value())
+console.log('here')
