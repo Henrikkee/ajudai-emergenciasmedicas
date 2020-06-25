873365e929a1e8b4ad7fad9e3082c4b1d04e58e7
Pedro Monteiro Guimaraes <pedromonteirogui@gmail.com>
2020-06-25 20:08:39 -0300

    Inicio preenchimendo

--- js/index.js
+++ js/index.js
@@ -1,162 +1,181 @@
-window.onload = () => {
-  const dados = [
+var adapter = new LocalStorage('db');
+var db = low(adapter);
+
+db.defaults({
+  items: [
     {
-      titulo: 'Acidente de carro',
-      img: 'img/justlogo.png',
+      name: 'Intoxicações',
+      sections: [
+        {
+          nome: 'Intoxicações',
+          articles: [
+            {
+              title: 'Intoxicação alimentar',
+              content:
+                'Intoxicação alimentar é uma doença que tem como causa a ingestão de alimentos ou bebidas contaminados, seja por fungos, bactérias, microrganismos ou vírus que afetam o sistema digestivo. E nesse sentido, uma dúvida muito comum é como tratar intoxicação alimentar.',
+              sintomas: [
+                'náuseas',
+                'vômitos',
+                'febre',
+                'dor abdominal ou cólicas',
+              ],
+              tratamento: {
+                content:
+                  'O tratamento da intoxicação alimentar, na maioria dos casos, é caseiro. Ele inclui a ingestão de muitos líquidos e uma alimentação equilibrada, leve e pobre em gorduras. Essas recomendações devem ser seguidas até que os sintomas desapareçam por completo. Confira, a seguir, os alimentos mais recomendados para tratar a intoxicação alimentar:',
+                alimentos: [
+                  'mingau de amido de milho',
+                  'chás com açúcar, mas sem cafeína. Evite o chá-preto, o chá-mate e o chá-verde',
+                  'maçã e pera cozidas e sem casca',
+                  'banana',
+                  'cenoura e batata cozidas',
+                  'arroz branco ou massa sem molhos ou gorduras',
+                  'frango grelhado ou cozido',
+                ],
+              },
+              image: 'image content',
+            },
+          ],
+        },
+      ],
     },
     {
-      titulo: 'Acidente de carro',
-      img: 'img/justlogo.png',
+      name: 'Quedas',
+      sections: [
+        {
+          nome: 'Quedas',
+          articles: [
+            {
+              title: 'Quedas domesticas',
+              content:
+                'Quedas são uma das maiores causas de acidentes domésticos. Podem ocorrer com crianças que amam escalar tudo que veem pela frente, e com idosos, principalmente devido a escadas sem corrimão ou pisos escorregadios.',
+              tratamentos: {
+                Ferimento:
+                  'lave bem a área afetada com água e sabão para evitar que a ferida infeccione',
+                Dor:
+                  ' Imobilize a área afetada pela queda e leve-a imediatamente para um pronto-socorro',
+                Desmaio:
+                  'Verifique a respiração e o pulso. Chame o resgate imediatamente',
+                ChoqueComChao:
+                  'Existe grande possibilidade de uma lesão interna. Neste caso, movimente a vítima o menos possível e chame o resgate imediatamente',
+              },
+              image: 'image content',
+            },
+          ],
+        },
+      ],
     },
-  ];
+    {
+      name: 'Queimaduras',
+      sections: [
+        {
+          nome: 'Queimaduras',
+          articles: [
+            {
+              title: 'Queimaduras',
+              text:
+                'Álcool, isqueiros e fósforos são perigosos e podem causar queimaduras graves, principalmente em crianças que estão em idade de querer explorar tudo. Também é preciso ter muito cuidado com idosos que têm acesso a fogão e eletroportáteis, por exemplo. Acidentes nesses contextos, infelizmente, são bastante comun',
+              tratamento:
+                'Lave a região com água fria e nada mais, A água fria — não gelada! — ajuda a interromper o processo da queimadura de duas formas: retirando os resíduos quentes da pele e resfriando a região. Por isso, este é o primeiro passo a se tomar quando ocorre uma queimadura.',
+              content:
+                'Após interromper o processo de queimadura e resfriar a região, é importante avaliar a intensidade da queimadura, que depende da temperatura do agente causador e do tempo em que a pele ficou exposta a ele. Há 3 graus de queimadura. O primeiro é o mais leve e atinge a camada superficial da pele, causando avermelhamento, coceira e dor. Em cerca de 3 dias, os sintomas desaparecem, embora a queimadura possa deixar uma mancha ou cicatriz na pele. Queimaduras de 2º grau atingem camadas mais profundas da pele, causando também inchaço e bolhas. É importante se lembrar de não estourar as bolhas para evitar infecções e procurar um médico para drenar as bolhas e avaliar a situação. Queimaduras de 3º grau precisam de cuidado especializado imediato, por isso o serviço de atendimento de emergência deve ser acionado. Elas atingem todas as camadas da pele, provocando a carbonização da área atingida.',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Choques elétricos',
+      sections: [
+        {
+          nome: 'Choques elétricos',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Afogamentos',
+      sections: [
+        {
+          nome: 'Afogamentos',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Outros acidentes domésticos',
+      sections: [
+        {
+          nome: 'Outros acidentes domésticos',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Depressão',
+      sections: [
+        {
+          nome: 'Atropelamento',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Diabetes',
+      sections: [
+        {
+          nome: 'Atropelamento',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Asma',
+      sections: [
+        {
+          nome: 'Atropelamento',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}).write();
 
-  window.localStorage.setItem('dados', JSON.stringify(dados));
-
-  hospitalForm.onsubmit = () => {
-    return console.log('funciona');
-  };
-};
-
-var adapter = new LocalStorage('db')
-var db = low(adapter)
-
-db.defaults({ items: [
-  {
-    name: 'Acidente de Carro',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'Sangramento',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'Doença Respiratória',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'Doença Cardiaca',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'Pressão Alta/Baixa',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'Alergia',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'Depressão',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'Diabetes',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    name: 'Asma',
-    sections: [
-      {
-        nome: 'Atropelamento',
-        articles: [
-          {
-            title: 'Conteudo Teste',
-            content: 'Conteudo do artigo',
-            image: 'image content'
-          }
-        ]
-      }
-    ]
-  },
-]}).write()
-
-console.log(db.get('items').value())
-console.log('here')
+console.log(db.get('items').value());
+console.log('here');
