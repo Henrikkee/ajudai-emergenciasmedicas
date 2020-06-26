1c03f73bdb4e182101d213efffb3a960cdb1238e
Henrique Monteiro <henrique.mont011@gmail.com>
2020-06-25 22:09:00 -0300

    Started javascript section page  code

---
+++ js/db.js
@@ -0,0 +1,191 @@
+let items = [
+    {
+      name: 'Intoxicações',
+      color: '#ffb7ff',
+      icon: 'fas fa-bong',
+      sections: [
+        {
+          nome: 'Intoxicações',
+          articles: [
+            {
+              title: 'Intoxicação alimentar',
+              content:
+                'Intoxicação alimentar é uma doença que tem como causa a ingestão de alimentos ou bebidas contaminados, seja por fungos, bactérias, microrganismos ou vírus que afetam o sistema digestivo. E nesse sentido, uma dúvida muito comum é como tratar intoxicação alimentar.',
+              sintomas: [
+                'náuseas',
+                'vômitos',
+                'febre',
+                'dor abdominal ou cólicas',
+              ],
+              tratamento: {
+                content:
+                  'O tratamento da intoxicação alimentar, na maioria dos casos, é caseiro. Ele inclui a ingestão de muitos líquidos e uma alimentação equilibrada, leve e pobre em gorduras. Essas recomendações devem ser seguidas até que os sintomas desapareçam por completo. Confira, a seguir, os alimentos mais recomendados para tratar a intoxicação alimentar:',
+                alimentos: [
+                  'mingau de amido de milho',
+                  'chás com açúcar, mas sem cafeína. Evite o chá-preto, o chá-mate e o chá-verde',
+                  'maçã e pera cozidas e sem casca',
+                  'banana',
+                  'cenoura e batata cozidas',
+                  'arroz branco ou massa sem molhos ou gorduras',
+                  'frango grelhado ou cozido',
+                ],
+              },
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Quedas',
+      color: '#bfbbff',
+      icon: 'fas fa-hiking',
+      sections: [
+        {
+          nome: 'Quedas',
+          articles: [
+            {
+              title: 'Quedas domesticas',
+              content:
+                'Quedas são uma das maiores causas de acidentes domésticos. Podem ocorrer com crianças que amam escalar tudo que veem pela frente, e com idosos, principalmente devido a escadas sem corrimão ou pisos escorregadios.',
+              tratamentos: {
+                Ferimento:
+                  'lave bem a área afetada com água e sabão para evitar que a ferida infeccione',
+                Dor:
+                  ' Imobilize a área afetada pela queda e leve-a imediatamente para um pronto-socorro',
+                Desmaio:
+                  'Verifique a respiração e o pulso. Chame o resgate imediatamente',
+                ChoqueComChao:
+                  'Existe grande possibilidade de uma lesão interna. Neste caso, movimente a vítima o menos possível e chame o resgate imediatamente',
+              },
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Queimaduras',
+      color: '#F6C2A0',
+      icon: 'fas fa-burn',
+      sections: [
+        {
+          nome: 'Queimaduras',
+          articles: [
+            {
+              title: 'Queimaduras',
+              text:
+                'Álcool, isqueiros e fósforos são perigosos e podem causar queimaduras graves, principalmente em crianças que estão em idade de querer explorar tudo. Também é preciso ter muito cuidado com idosos que têm acesso a fogão e eletroportáteis, por exemplo. Acidentes nesses contextos, infelizmente, são bastante comun',
+              tratamento:
+                'Lave a região com água fria e nada mais, A água fria — não gelada! — ajuda a interromper o processo da queimadura de duas formas: retirando os resíduos quentes da pele e resfriando a região. Por isso, este é o primeiro passo a se tomar quando ocorre uma queimadura.',
+              content:
+                'Após interromper o processo de queimadura e resfriar a região, é importante avaliar a intensidade da queimadura, que depende da temperatura do agente causador e do tempo em que a pele ficou exposta a ele. Há 3 graus de queimadura. O primeiro é o mais leve e atinge a camada superficial da pele, causando avermelhamento, coceira e dor. Em cerca de 3 dias, os sintomas desaparecem, embora a queimadura possa deixar uma mancha ou cicatriz na pele. Queimaduras de 2º grau atingem camadas mais profundas da pele, causando também inchaço e bolhas. É importante se lembrar de não estourar as bolhas para evitar infecções e procurar um médico para drenar as bolhas e avaliar a situação. Queimaduras de 3º grau precisam de cuidado especializado imediato, por isso o serviço de atendimento de emergência deve ser acionado. Elas atingem todas as camadas da pele, provocando a carbonização da área atingida.',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Choques elétricos',
+      color: '#97FFD8',
+      icon: 'fas fa-bolt',
+      sections: [
+        {
+          nome: 'Choques elétricos',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Afogamentos',
+      color: '#D4D2FF',
+      icon: 'fas fa-swimmer',
+      sections: [
+        {
+          nome: 'Afogamentos',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Outros acidentes domésticos',
+      color: '#D2EAFF',
+      icon: 'fas fa-house-damage',
+      sections: [
+        {
+          nome: 'Outros acidentes domésticos',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Depressão',
+      color: '#E2D2FF',
+      icon: 'fas fa-sad-tear',
+      sections: [
+        {
+          nome: 'Atropelamento',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Diabetes',
+      color: '#FFA2AF',
+      icon: 'fas fa-cookie',
+      sections: [
+        {
+          nome: 'Atropelamento',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Asma',
+      color: '#D2E7FF',
+      icon: 'fas fa-lungs',
+      sections: [
+        {
+          nome: 'Atropelamento',
+          articles: [
+            {
+              title: 'Conteudo Teste',
+              content: 'Conteudo do artigo',
+              image: 'image content',
+            },
+          ],
+        },
+      ],
+    },
+  ];
---
+++ js/secao.js
@@ -0,0 +1,26 @@
+$(function() {
+    let params = new URLSearchParams(location.search);
+    let query = params.get('name')
+    let title = ''
+    let icon = null
+    let secao = []
+    let randomColors = ['#ffb7ff','#bfbbff','#f6c2a0','#97ffd8','#75d2af']
+    items.forEach(function(val) { 
+        if(val.name == query) {
+            title = val.name
+            icon = val.icon
+            secao = val.sections
+            $("#lblSecaoTitle").text(title);
+            $(".breadcrumb-current").text(title);
+            console.log(secao[0])
+            secao[0].articles.forEach(function(val) { 
+                const randomElement = randomColors[Math.floor(Math.random() * randomColors.length)];
+                let item = $(".item-first").clone();
+                item.removeClass('d-none').removeClass('item-first').appendTo(".content-list");
+                item.find('.item-title').text(val.title);
+                item.find('a').attr("href",'artigo.html');
+                item.find('.item-icon').css('background', randomElement).addClass(icon);
+            })
+        }
+    })
+});
